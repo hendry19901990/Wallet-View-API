362b853c6350502f47d9bdcc840a8dd087c15234
Hendry Rodriguez <hendryrodriguez1990@gmail.com>
2023-08-10 06:55:50 -0600

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ import 'dotenv/config';
 const app = express();
 const API_TOKEN = process.env.API_TOKEN;
 const ANKR_BASE_URL = `https://rpc.ankr.com/multichain/${process.env.API_TOKEN}`;
-console.log(ANKR_BASE_URL);
 
 app.get('/wallet/balance', async (req, res) => {
     const wallet_address = req.query.wallet_address;
@@ -18,49 +17,44 @@ app.get('/wallet/balance', async (req, res) => {
 
     try {
 
-        // Fetch wallet balance
-        const accountBalanceResponse = await axios.post(`${ANKR_BASE_URL}/?ankr_getAccountBalance=`, {
+        let req = {
             id: 1,
             jsonrpc: '2.0',
             method: 'ankr_getAccountBalance',
             params: {
                 walletAddress: `${wallet_address}`
             }
-        },
+        };
+
+        // Fetch wallet balance
+        const accountBalanceResponse = await axios.post(`${ANKR_BASE_URL}/?ankr_getAccountBalance=`, req,
          { headers });
         const accountBalance = accountBalanceResponse.data.result;
 
-        // Fetch token prices
-        const tokenPriceResponse = await axios.post(
-            `${ANKR_BASE_URL}/?ankr_getTokenPrice=`,
-            {
-                id: 1,
-                jsonrpc: "2.0",
-                method: "ankr_getTokenPrice",
-                params: {
-                    blockchain: `eth`
-                }
-            },
-            {headers}
-        );
-        const tokenPrices = tokenPriceResponse.data.result;
+        let tokenPrice = accountBalance.assets.find(asset => {
+            const symbol = asset.tokenSymbol? asset.tokenSymbol: asset.symbol;
+            return symbol === cryptocurrency;
+        })
+
+        if(!tokenPrice)
+           return res.status(404).json({ error: cryptocurrency + " not found!" });
+
 
         // Calculate values and build response  
         const balances = accountBalance.assets.map(asset => {
             const symbol = asset.tokenSymbol? asset.tokenSymbol: asset.symbol;
-            const balance = asset.balance;
-            const priceInCryptocurrency = tokenPrices.usdPrice;
-            const valueInCryptocurrency = balance * priceInCryptocurrency;
+            const balance = Number.parseFloat(asset.balance);
+            const priceInCryptocurrency = tokenPrice.tokenPrice;
+            const valueInCryptocurrency = (symbol === cryptocurrency && priceInCryptocurrency > 0) ? balance: priceInCryptocurrency / balance;
+            
             return {
-                symbol,
-                balance,
-                value_in_cryptocurrency: valueInCryptocurrency
+                balance: balance + ` ${symbol}`,
+                valueInCryptocurrency: valueInCryptocurrency + ` ${cryptocurrency}`,
             };
         });
 
         const response_data = {
             wallet_address,
-            cryptocurrency,
             balances
         };
         
